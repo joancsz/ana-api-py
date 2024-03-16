e5ad99dd1f83c72997a9f35aff8ce9bd3cbc4aab
Joan <joancsz01@gmail.com>
2024-03-15 22:18:47 -0300

    feat: add a Check

--- error/error.py
+++ error/error.py
@@ -1,9 +1,26 @@
+from lxml import etree
+
 class NotFoundError(Exception):
     def __init__(self, info="Response <404>: File was not found in the url"):
         self.info = info
         super().__init__(self.info)
 
-class EmptyMandatoryParameter(Exception):
-    def __init__(self, info="Please fill in the mandatory parameters"):
+class NoDataAvailable(Exception):
+    def __init__(self, info="No data avaiable"):
         self.info = info
         super().__init__(self.info)
+
+class Check:
+
+    def __init__(self, content) -> None:
+        self.check_error(content) 
+
+    def check_error(self, content):
+        message = False
+        root = etree.fromstring(content)
+        for table in root.findall(".//ErrorTable"):
+            message = table.findtext('Error')
+
+        if message:
+            raise NoDataAvailable(message)
+
